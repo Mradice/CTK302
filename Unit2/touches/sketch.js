40d44e17b8e7327653d2fb89172b41249591f590
Mradice <mradice@ilstu.edu>
2022-10-12 13:59:02 -0500

    commit

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -1,8 +1,12 @@
 let numberOfTouches ;
+let i1, i2, i3 ;
 
 function setup() {
   createCanvas(400, 400);
-  //imageMode
+  i1 = loadImage("assets/1.jpg") ;
+  i2 = loadImage("assets/2.jpg") ;
+  i3 = loadImage("assets/3.jpg") ;
+  imageMode (CENTER);
 }
 
 function draw() {
@@ -17,6 +21,7 @@ function draw() {
       
       case 1: 
        text("it's kind of lonely here", 5, 22) ; 
+       image(i1, width/2, height/2 - 120, 100, 100) ;
       // put a picture here change text to your own words
       break ;
       
